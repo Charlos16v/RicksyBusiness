55f3e6c23f4d83a1f9eed1e0db806fbafb4399a6
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-02 14:25:26 +0100

    test: CrystalExpenderTest

--- src/test/java/edu/pingpong/domain/CrystalExpenderTest.java
+++ src/test/java/edu/pingpong/domain/CrystalExpenderTest.java
@@ -3,19 +3,29 @@ package edu.pingpong.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class CrystalExpenderTest {
 
     CrystalExpender crystalExpender = null;
+    CreditCard card = null;
 
     @Before
     public void setupCrystalExpender() {
         crystalExpender = new CrystalExpender(4, 50);
+        card = new CreditCard("Master", "8888");
     }
 
     @Test
     public void constructorTest() {
         assertNotNull(crystalExpender);
     }
+
+    @Test
+    public void dispatchTest() {
+        crystalExpender.dispatch(card);
+        assertEquals(2950, card.credit(), 0);
+    }
 }
+
