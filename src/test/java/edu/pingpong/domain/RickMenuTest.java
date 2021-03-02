729abff64628a2e0a8cedbca5238b59d41af4f41
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-02 14:25:58 +0100

    test: RickMenuTest

---
+++ src/test/java/edu/pingpong/domain/RickMenuTest.java
@@ -0,0 +1,31 @@
+package edu.pingpong.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class RickMenuTest {
+
+
+    RickMenu rickMenu = null;
+    CreditCard card = null;
+
+    @Before
+    public void setupRickMenu() {
+        rickMenu = new RickMenu( 10, 10);
+        card = new CreditCard("Master", "8888");
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(rickMenu);
+    }
+
+    @Test
+    public void dispatchTest() {
+        rickMenu.dispatch(card);
+        assertEquals(2990, card.credit(), 0);
+    }
+}
